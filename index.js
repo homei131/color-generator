ff81393f678d72f775f15bc5d4577277468d3e80
homei131 <122817746+homei131@users.noreply.github.com>
2023-02-07 04:04:02 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector("#btn");
+	 
+	 function generateColor() {
+	 const hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+	 let code = "";
+	 for(let i = 0; i < 6; i++) {
+	 code += hexArray[Math.floor(Math.random()*15)];
+	 }
+	 return `#${code}`
+	 }
+	 
+	 btn.addEventListener('click', () => {
+	 document.querySelector('.box').style.background = generateColor();
+	 document.querySelector('.hexColor').innerHTML = generateColor();
+	 });
